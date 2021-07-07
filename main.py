e8c821ce5aea4170de7ad299c604ddd96e56f2ba
Aaron <aaron@xnovax.net>
2021-07-06 20:12:47 -0400

    Added some comments and fixed wait times

--- main.py
+++ main.py
@@ -8,45 +8,43 @@ from selenium import webdriver
 
 import config
 
-WAIT_TIME = 4
+WAIT_TIME = 2
 
 
 def main():
+    # Setup the web driver
     driver = webdriver.Safari()
     driver.get('https://lightroom.adobe.com/signin')
     driver.set_window_size(2000, 1200)
 
     time.sleep(WAIT_TIME)
 
+    # Login
     email_field = driver.find_element_by_xpath('//*[@id="EmailPage-EmailField"]')
     email_field.send_keys(config.LIGHTROOM_EMAIL)
-
     time.sleep(WAIT_TIME)
-
     continue_button = driver.find_element_by_xpath('//*[@id="EmailForm"]/section[2]/div[2]/button/span').click()
-
     time.sleep(WAIT_TIME)
-
     password_field = driver.find_element_by_xpath('//*[@id="PasswordPage-PasswordField"]')
     password_field.send_keys(config.LIGHTROOM_PASSWORD)
     continue_button = driver.find_element_by_xpath('//*[@id="PasswordForm"]/section[2]/div[2]/button/span').click()
-
     time.sleep(WAIT_TIME * 2)
-
     enable_cookies_button = driver.find_element_by_xpath('//*[@id="onetrust-accept-btn-handler"]').click()
+    time.sleep(WAIT_TIME)
 
-    time.sleep(WAIT_TIME * 2)
-
+    # Click into the all photos tab
     all_photos_button = driver.find_element_by_xpath('//*[@id="ze-sidebar-all-photos"]/div[1]/span[2]').click()
 
+    # Click on the first image to expand it
     first_image_tile = driver.find_elements_by_class_name('image')[0].click()
-    time.sleep(3)
+    time.sleep(WAIT_TIME)
 
     # Set requests cookies from webdriver
     s = requests.Session()
     for cookie in driver.get_cookies():
         s.cookies.set(cookie['name'], cookie['value'])
 
+    # Loop through the catalog of images
     for _ in range(50):
         # Find the correct image
         div_tag = driver.find_element_by_class_name('ze-active')
